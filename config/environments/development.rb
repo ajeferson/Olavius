0908636370d23d8e078858e878e76dd621d9a18f
Alan Jeferson <alan.jeferson11@gmail.com>
2016-08-31 23:03:21 -0300

    Reset passwrod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,24 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Action Mailer's config
+  config.action_mailer.default_url_options = {
+      host: '127.0.0.1',
+      port: '3000'
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 465,
+      user_name:            'olavius.social@gmail.com',
+      password:             'welcometovalhalla',
+      authentication:       'plain',
+      ssl:                  :true,
+      tls:                  :true,
+      enable_starttls_auto: true
+  }
+
+  config.web_console.whiny_requests = false
+
 end
