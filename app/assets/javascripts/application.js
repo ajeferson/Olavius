e62736ff2d9bbe1154a16762a7c36447419f3ea0
Alan Jeferson <alan.jeferson11@gmail.com>
2016-09-10 20:12:39 -0300

    Organizing template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require template/Chart
 //= require jquery
 //= require bootstrap
 //= require template/jquery.dcjqaccordion.2.7
