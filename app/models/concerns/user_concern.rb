02667dc6a983e67d104d72ad6205022a4c2a8d15
Alan Jeferson <alan.jeferson11@gmail.com>
2016-09-17 14:42:12 -0300

    Invitations scopes

--- app/models/concerns/user_concern.rb
+++ app/models/concerns/user_concern.rb
@@ -1,15 +1,23 @@
 module UserConcern extend ActiveSupport::Concern
 
-  def short_name
-    name.split().first
-  end
+def short_name
+  name.split().first
+end
 
-  def friends
-    self.friends_a + self.friends_b
-  end
+def friends
+  self.friends_a + self.friends_b
+end
 
-  def is_friend_with?(user)
-    self.friends.include?(user)
-  end
+def invitations
+  self.host_invitations + self.guest_invitations
+end
+
+def invitation_users
+  self.invitation_guest_users + self.invitation_host_users
+end
+
+def is_friend_with?(user)
+  self.friends.include?(user)
+end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,18 @@ class User < ActiveRecord::Base
   mount_uploader :profile_picture, ProfilePictureUploader
 
   has_many :posts, dependent: :destroy
-  has_many :guest_invitations, foreign_key: 'invitee_id', class_name: 'Invitation'
-  has_many :host_invitations, foreign_key: 'inviting_id', class_name: 'Invitation'
-  has_many :friendships_a, foreign_key: 'user_a_id', class_name: 'Friendship'
-  has_many :friendships_b, foreign_key: 'user_b_id', class_name: 'Friendship'
-  has_many :friends_b, through: :friendships_a, source: 'user_b', class_name: 'User'
-  has_many :friends_a, through: :friendships_b, source: 'user_a', class_name: 'User'
+
+  # Invitations
+  has_many :guest_invitations, foreign_key: 'invitee_id', class_name: 'Invitation'                     # Invitations this received
+  has_many :host_invitations, foreign_key: 'inviting_id', class_name: 'Invitation'                     # Invitations this user made
+  has_many :invitation_host_users, through: :guest_invitations, source: 'inviting', class_name: 'User' # Users that invited this user
+  has_many :invitation_guest_users, through: :host_invitations, source: 'invitee', class_name: 'User'  # Users that were invited by this user
+
+  # Friends
+  has_many :friendships_a, foreign_key: 'user_a_id', class_name: 'Friendship'                          # Friendships by the user_a relation
+  has_many :friendships_b, foreign_key: 'user_b_id', class_name: 'Friendship'                          # Friendships by the user_b relation
+  has_many :friends_b, through: :friendships_a, source: 'user_b', class_name: 'User'                   # Friends by the user_b relation
+  has_many :friends_a, through: :friendships_b, source: 'user_a', class_name: 'User'                   # Friends by the user_a relation
 
   scope :search, -> (query)  {
     where('lower(name) LIKE :query OR lower(email) LIKE :query',
